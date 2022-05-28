26b2d09b30daa1e182f8ef5d67b260ce9c7e066b
zhaoyy618 <1553103832@qq.com>
2022-05-29 00:22:22 +0800

    update:master修改addText文件,添加fruits

--- first/src/main/java/com/example/first/controller/HtmlController.java
+++ first/src/main/java/com/example/first/controller/HtmlController.java
@@ -2,15 +2,23 @@ package com.example.first.controller;
 
 
 import io.swagger.annotations.Api;
+import lombok.extern.log4j.Log4j2;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Log4j2
 @Controller
 @Api("html")
 public class HtmlController {
-
+    Logger logger = LoggerFactory.getLogger(HtmlController.class);
     @GetMapping("/index")
     public String index(){
+        logger.error("11111");
+        logger.debug("222222");
+        logger.info("44444");
+       log.error("qwqw");
         return "index";
     }
 }
--- first/src/main/java/com/example/first/controller/XingLangController.java
+++ first/src/main/java/com/example/first/controller/XingLangController.java
@@ -7,6 +7,8 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,7 +28,7 @@ public class XingLangController {
 
     @Resource
     private FeignTimeOutSingle feignTimeOutSingle;
-
+    Logger logger = LoggerFactory.getLogger(XingLangController.class);
 
     @ApiOperation(value = "上传文件接口",notes = "上传文件接口")
     @ApiImplicitParams({
@@ -40,11 +42,19 @@ public class XingLangController {
     @GetMapping("/timeOut")
     public void timeOut(String id, Integer date){
         String timeOut = feignTimeOut.getTimeOut();
+        logger.error("11111");
+        logger.debug("222222");
+        logger.info("2121");
+        logger.info("44444");
         System.out.println(timeOut);
     }
     @GetMapping("/timeOutSingle")
     public void timeOutSingle(String id, Integer date){
         String timeOut = feignTimeOutSingle.getTimeOutSingle();
+        logger.error("11111");
+        logger.debug("222222");
+        logger.info("2121");
+        logger.info("44444");
         System.out.println(timeOut);
     }
 }
